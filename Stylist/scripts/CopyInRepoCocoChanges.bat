8bf24642633df2ce9806ef938b0841e53243f59c
jackson <63016612+fairybow@users.noreply.github.com>
2024-12-07 23:19:25 -0500

    Update CopyInRepoCocoChanges.bat

--- Stylist/scripts/CopyInRepoCocoChanges.bat
+++ Stylist/scripts/CopyInRepoCocoChanges.bat
@@ -2,6 +2,10 @@
 
 call "WinGitRoot.bat"
 
-xcopy /y /d /e /i "Stylist\submodules\Coco\include\" "C:\Dev\Coco\include\"
-xcopy /y /d /e /i "Stylist\submodules\Coco\src\" "C:\Dev\Coco\src\"
-xcopy /y /d /e /i "Stylist\submodules\Coco\old\" "C:\Dev\Coco\old\"
+rmdir /s /q "C:\Dev\Coco\include\"
+rmdir /s /q "C:\Dev\Coco\src\"
+rmdir /s /q "C:\Dev\Coco\old\"
+
+xcopy /y /e /i "Stylist\submodules\Coco\include\" "C:\Dev\Coco\include\"
+xcopy /y /e /i "Stylist\submodules\Coco\src\" "C:\Dev\Coco\src\"
+xcopy /y /e /i "Stylist\submodules\Coco\old\" "C:\Dev\Coco\old\"
